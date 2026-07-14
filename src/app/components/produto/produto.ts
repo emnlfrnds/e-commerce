97ba236aa131b65739d28e948487d19c2e2f4a19
emnlfrnds <enzod2716@gmail.com>
2026-07-14 14:36:01 -0300

    refactor(produto.ts): criação de atributos booleanos e array de objetos para produto

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
+import { CurrencyPipe, UpperCasePipe } from '@angular/common';
+import { MeuPipePipe } from '../../meu-pipe-pipe';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe, CurrencyPipe, MeuPipePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto
 {
+  // produto = "Monitor";
+  // valor = 900;
+  // mostrarProduto = true;
+  // mostrarValor = true;
 
-  produto = "Monitor";
-  valor = 900;
-  mostrarProduto = true;
   mostrarValor = true;
-
+  mostrarProduto = true;
+  produtos = [
+    {produto: 'monitor', valor: 799},
+    {produto: 'teclado', valor: 29.90},
+    {produto: 'mouse', valor: 39.90},
+  ];
 }
