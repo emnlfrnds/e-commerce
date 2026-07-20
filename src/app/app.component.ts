00a841250a02c838dfec69ab4ee12dec2ee87cde
emnlfrnds <enzod2716@gmail.com>
2026-07-20 17:23:58 -0300

    feat: adiciona rotas de link, adição da rota carrinho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { Produto } from './features/produtos/produto/produto.component';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos.component';
+// import { Produto } from './features/produtos/produto/produto.component';
+// import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  loja = "LOVI - Sua Loja Virtual!";
 }
