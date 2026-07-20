c07c4fc509cf7fc24a007c72996a57a423db02b5
emnlfrnds <enzod2716@gmail.com>
2026-07-20 14:01:45 -0300

    feat(lista-produtos): adiciona função construtora no código

--- src/app/features/produtos/lista-produtos/lista-produtos.component.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.component.ts
@@ -1,10 +1,12 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, signal, computed, effect } from '@angular/core';
 import { Produto } from '../produto/produto.component';
+import { UpperCasePipe } from '@angular/common';
 import { valorFormatadoPipe } from '../../../shared/pipes/valor-formatado-pipe';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto, valorFormatadoPipe],
+  imports: [Produto, valorFormatadoPipe, UpperCasePipe],
   templateUrl: './lista-produtos.component.html',
   styleUrl: './lista-produtos.component.css',
 })
@@ -46,4 +48,35 @@ export class ListaProdutos {
       {nome: 'Arroz Fazenda', valor: 2.49},
     ]);
   }
+
+construtor() {
+  effect(() => {
+    console.log("Lista de Produtos Alterados: ", this.produtos());
+  });
+  effect(() => {
+    console.log("Valor total atualizado: ", this.valorTotal());
+  });
+  effect(() => {
+    if (typeof document !== "undefined") {
+        document.title = `(${this.totalProdutos()}) Minha Loja`;
+    }
+  });
+}
+
+  produtoSelecionado = signal <string | null> (null);
+
+  carrinho = signal <{nome: string; valor: number}[]>([]);
+
+  quantidadeCarrinho = computed(() => this.carrinho().length);
+
+  totalCarrinho = computed(() => { return this.carrinho().reduce((total, item) => total + item.valor, 0)});
+
+  adicionarAoCarrinho (produto: {nome: string; valor: number}) {
+    this.carrinho.update(listaAtual => [...listaAtual, produto]);
+  }
+
+  removeTodosProdutos() {
+    this.produtos.set([])
+  }
+
 }
