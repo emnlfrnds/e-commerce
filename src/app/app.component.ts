e7cf032003fa3196db902c47582a44ced591ea52
emnlfrnds <enzod2716@gmail.com>
2026-07-21 16:37:13 -0300

    style(app): remove comentários desnecessários

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
-// import { Produto } from './features/produtos/produto/produto.component';
-// import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, UpperCasePipe],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('e-commerce');
