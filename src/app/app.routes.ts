9b6684d43d170e6a5c95a09ab1e3654fcbe5a8dd
emnlfrnds <enzod2716@gmail.com>
2026-07-21 16:38:28 -0300

    refactor(app.routes): aprimora as rotas dos componentes utilizando a função loadComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,45 @@
-import { Routes } from '@angular/router';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos.component';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
+// import { Routes } from '@angular/router';
+// import { Home } from './pages/home/home';
+// import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos.component';
+// import { Carrinho } from './features/carrinho/carrinho/carrinho';
+
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     component: Home,
+//   },
+//   {
+//     path: 'produtos',
+//     component: ListaProdutos,
+//   },
+//   {
+//     path: 'carrinho',
+//     component: Carrinho,
+//   }
+// ];
+
+//* Código Final LazyLoading e LoadComponent
+
+import { Routes } from "@angular/router";
 
 export const routes: Routes = [
   {
     path: '',
-    component: ListaProdutos,
+    loadComponent: () =>
+      import('./pages/home/home').then(m => m.Home)
+  },
+  {
+    path: 'produtos',
+    loadComponent: () =>
+      import('./features/produtos/lista-produtos/lista-produtos.component').then(m => m.ListaProdutos)
   },
   {
     path: 'carrinho',
-    component: Carrinho,
+    loadComponent: () =>
+      import('./features/carrinho/carrinho/carrinho.component').then(m => m.Carrinho)
+  },
+  {
+    path:'**',
+    redirectTo: '',
   }
 ];
