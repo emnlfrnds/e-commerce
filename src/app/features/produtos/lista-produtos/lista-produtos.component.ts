cb143e8fc4647491202c6f952c606ee12170c8f2
emnlfrnds <enzod2716@gmail.com>
2026-07-22 17:31:29 -0300

    refactor(lista-produtos): adaptação do sistema de produtos para API

--- src/app/features/produtos/lista-produtos/lista-produtos.component.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.component.ts
@@ -2,55 +2,67 @@ import { Component, signal, computed, effect } from '@angular/core';
 import { Produto } from '../produto/produto.component';
 import { UpperCasePipe } from '@angular/common';
 import { valorFormatadoPipe } from '../../../shared/pipes/valor-formatado-pipe';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-lista-produtos',
   imports: [Produto, valorFormatadoPipe, UpperCasePipe],
   templateUrl: './lista-produtos.component.html'
 })
-export class ListaProdutos {
-  produtos = signal([
-    {
-      nome: 'Teclado Gamer',
-      valor: 127.90},
-    {
-      nome: 'Mouse Gamer',
-      valor: 69.90},
-    {
-      nome: 'Mousepad Gamer',
-      valor: 25.90},
-    {
-      nome: 'Headset Gamer',
-      valor: 259.99},
-    {
-      nome: 'Controle Xbox',
-      valor: 368.00},
-  ]);
 
-  exibirProduto(nome: string) {
-    console.log ('Produto Selecionado: ', nome);
+export class ListaProdutos {
+
+  produtos = signal<{ nome: string; valor: number }[]>([]);
+  carregando = signal(true);
+
+  carregarProdutos() {
+    this.carregando.set(true);
+    this.http.get<
+      { title: string; valor: number }[]
+    >
+      ('https://fakstore.com/products')
+      .subscribe({
+        next: (dados) => {
+          const produtosFormatados = dados.map(p => ({
+            nome: p.title, valor: p.valor
+          }));
+          this.produtos.set(produtosFormatados);
+          this.carregando.set(false);
+        },
+        error: (e) => {
+          console.error('Erro ao carregar produtos: ', e);
+          this.carregando.set(false);
+        }
+      })
   }
 
   // Funcionalidades
 
+  exibirProduto(nome: string) {
+    console.log('Produto Selecionado: ', nome);
+  }
+
   adicionarProduto() {
     this.produtos.update(ListaAtual => [
-      ...ListaAtual, {nome: 'Sony Playstation 5', valor: 7599}
+      ...ListaAtual, { nome: 'Sony Playstation 5', valor: 7599 }
     ]);
   }
 
   substituirProdutos() {
     this.produtos.set([
-      {nome: 'Arroz Fazenda', valor: 2.49},
+      { nome: 'Arroz Fazenda', valor: 2.49 },
     ]);
   }
 
   // Informações
 
   totalProdutos = computed(() => this.produtos().length);
-  valorTotal = computed(() => { return this.produtos().reduce((total, item) => total + item.valor, 0)});
+  valorTotal = computed(() => { return this.produtos().reduce((total, item) => total + item.valor, 0) });
 
-  construtor() {
+  constructor(private http: HttpClient) {
+
+    this.carregarProdutos();
+
     effect(() => {
       console.log("Lista de Produtos Alterados: ", this.produtos());
     });
@@ -59,20 +71,20 @@ export class ListaProdutos {
     });
     effect(() => {
       if (typeof document !== "undefined") {
-          document.title = `(${this.totalProdutos()}) Minha Loja`;
+        document.title = `(${this.totalProdutos()}) Minha Loja`;
       }
     });
   }
 
-  produtoSelecionado = signal <string | null> (null);
+  produtoSelecionado = signal<string | null>(null);
 
-  carrinho = signal <{nome: string; valor: number}[]>([]);
+  carrinho = signal<{ nome: string; valor: number }[]>([]);
 
   quantidadeCarrinho = computed(() => this.carrinho().length);
 
-  totalCarrinho = computed(() => { return this.carrinho().reduce((total, item) => total + item.valor, 0)});
+  totalCarrinho = computed(() => { return this.carrinho().reduce((total, item) => total + item.valor, 0) });
 
-  adicionarAoCarrinho (produto: {nome: string; valor: number}) {
+  adicionarAoCarrinho(produto: { nome: string; valor: number }) {
     this.carrinho.update(listaAtual => [...listaAtual, produto]);
   }
 }
